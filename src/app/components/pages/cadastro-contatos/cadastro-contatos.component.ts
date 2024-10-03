8410e8a7710d32f8be231dc916658b8e26a5a033
Matheus Peluso <matheuspeluso17@gmail.com>
2024-10-03 20:36:21 -0300

    fineshed cadastro and consulta

--- src/app/components/pages/cadastro-contatos/cadastro-contatos.component.ts
+++ src/app/components/pages/cadastro-contatos/cadastro-contatos.component.ts
@@ -1,12 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-contatos',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './cadastro-contatos.component.html',
   styleUrl: './cadastro-contatos.component.css'
 })
 export class CadastroContatosComponent {
+
+  constructor(
+    private htttpClient: HttpClient
+  ){}
+
+  formulario_contato = new FormGroup({
+    nome: new FormControl('', [Validators.required]),
+    email: new FormControl('',[Validators.required]),
+    telefone: new FormControl('', [Validators.required])
+  });
+
+  cadastrarContato(){
+    this.htttpClient.post('http://localhost:8081/api/contatos',
+    this.formulario_contato.value,
+    {responseType: 'text'})
+    .subscribe({
+      next: (data) =>{
+        alert(data);
+      }
+    })
+  }
+
 
 }
--- src/app/components/pages/consulta-contatos/consulta-contatos.component.ts
+++ src/app/components/pages/consulta-contatos/consulta-contatos.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-consulta-contatos',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './consulta-contatos.component.html',
   styleUrl: './consulta-contatos.component.css'
 })
 export class ConsultaContatosComponent {
+
+  //atributo para guardar os dados que vem da requisição http
+  contatos: any[] = [];
+
+  constructor(
+    private httpClient: HttpClient
+  ){}
+  //função ngOinit é executada sempre que o componnete é abeto
+  ngOnInit(){
+    this.httpClient.get('http://localhost:8081/api/contatos')
+    .subscribe({
+      next: (data) =>{
+        this.contatos = data as any[];
+      }
+    });
+
+  }
 
 }
